e40ca233a95a66aeb393f4fea471da8cd1bd9696
MyPoZi <kxx555u.u@gmail.com>
2018-10-03 22:36:31 +0900

    add messagePush

--- test/main.js
+++ test/main.js
@@ -4,12 +4,24 @@ function initial() {
         data: {
             message: 'this is Vue.js sample!',
             inputData: '',
-            actionMessage: ''
+            actionMessage: '',
+            messageCss: '',
+            msgArray:[],
+            messagePush: '',
+        },
+        created: function() {
+            this.msgArray.push('sample message.');
+            this.messagePush = this.msgArray;
         },
         methods: {
             doAction: function () {
                 var str = this.text1;
                 this.actionMessage = 'you typed:' + str + '.';
+            },
+            doActionPush: function () {
+                this.msgArray.push(this.text2);
+                this.messagePush = this.msgArray;
+
             }
         }
     })
