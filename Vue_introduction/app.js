648e7cd84b5fd0b6bd5740847830097188bcf808
MyPoZi <kxx555u.u@gmail.com>
2018-10-25 19:23:42 +0900

    finished basics of vue

--- Vue_introduction/app.js
+++ Vue_introduction/app.js
@@ -2,12 +2,12 @@ var items = [
     {
         name: '鉛筆',
         price: '300',
-        quantity: 1
+        quantity: 0
     },
     {
         name: 'ノート',
         price: '400',
-        quantity: 1
+        quantity: 0
     },
     {
         name: '消しゴム',
@@ -19,7 +19,8 @@ var items = [
 var vm = new Vue({
     el: '#app',
     data: {
-        items: items
+        items: items,
+        messagePrefix: 'hello'
     },
     filters: {
         numberWithDelimiter: function (value) {
@@ -29,6 +30,14 @@ var vm = new Vue({
             return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
         },
     },
+    methods: {
+        doBuy: function () {
+            alert(this.totalPriceWithTax + '円のお買い上げ')
+            this.items.forEach(function (item) { //forEach(function(value, index, array)
+                item.quantity = 0
+            })
+        }
+    },
     computed: {
         totalPrice: function () {
             return this.items.reduce(function (sum, item) {
@@ -37,6 +46,24 @@ var vm = new Vue({
         },
         totalPriceWithTax: function () {
             return Math.floor(this.totalPrice * 1.08)
+        },
+        sumItems: function () {
+            return this.items.reduce(function (sum, item) {
+                return sum + item.quantity
+            }, 0)
+        },
+        canBuy: function () {
+            return this.totalPrice >= 1000
+        },
+        errorMessageStyle: function () {
+            return {
+                border: this.canBuy ? '' : '1px solid red',
+                color: this.canBuy ? '' : 'red'
+            }
+        },
+        message: function () {
+            var timestamp = Date.now()
+            return this.messagePrefix + ',' + timestamp
         }
     }
 })
