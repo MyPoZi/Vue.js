409ee4664198b8b088dcfe68416b8637d3a5b2af
MyPoZi <kxx555u.u@gmail.com>
2018-10-29 16:15:43 +0900

    add createUser

--- Vue_introduction/section4/main.js
+++ Vue_introduction/section4/main.js
@@ -20,7 +20,7 @@ var getUser = function (userId, callback) {
         var filteredUsers = userData.filter(function (user) {
             return user.id === parseInt(userId, 10)
         })
-        callback(null, filteredUsers && filteredUsers[0])
+        callback(null, filteredUsers && filteredUsers[0]) // filteredUsers必要??
     }, 1000)
 }
 
@@ -36,6 +36,88 @@ var userData = [
         description: 'プログラマです'
     }
 ]
+
+// 擬似的にAPI経由で情報を更新したようにする
+// 実際のWebアプリケーションではServerへPOSTリクエストを行う
+var postUser = function (params, callback) {
+    setTimeout(function () {
+        // idは追加されるごとに自動的にincrementされていく
+        params.id = userData.length + 1
+        userData.push(params)
+        callback(null, params)
+    }, 1000)
+}
+
+// ユーザ新規登録コンポーネント
+var UserCreate = {
+    template: `
+    <div>
+        <div class="sending" v-if="sending">Sending...</div>
+        <div>
+            <h2>新規ユーザー作成</h2>
+            <div>
+                <label>名前：</label>
+                <input type="text" v-model="user.name">
+            </div>
+            <div>
+                <label>説明文：</label>
+                <textarea v-model="user.description"></textarea>
+            </div>
+            <div v-if="error" class="error">
+                {{ error }}
+            </div>
+            <div>
+                <input type="button" @click="createUser" value="送信">
+            </div>
+        </div>
+    </div>
+    `,
+    data: function () {
+        return {
+            sending: false,
+            user: this.defaultUser(),
+            error: null
+        }
+    },
+
+    created: function () {
+    },
+
+    methods: {
+        defaultUser: function () {
+            return {
+                name: '',
+                description: ''
+            }
+        },
+
+        createUser: function () {
+            // 入力パラメータのバリデーション
+            if (this.user.name.trim() === '') {
+                this.error = 'Nameは必須です'
+                return
+            }
+            if (this.user.description.trim() === '') {
+                this.error = 'Descriptionは必須です'
+                return
+            }
+            postUser(this.user, (function (err, user) {
+                this.sending = false
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.error = null
+                    // デフォルトでフォームをリセット
+                    this.user = this.defaultUser()
+                    alert('新規ユーザーが登録されました')
+                    // ユーザ一覧ページに戻る
+                    this.$router.push('/users')
+                }
+            }).bind(this))
+        }
+    }
+}
+
 // 詳細ページのコンポーネント
 var UserDetail = {
     template: `
@@ -149,6 +231,10 @@ var router = new VueRouter({
         {
             path: '/users',
             component: UserList
+        },
+        {
+            path: '/users/new',
+            component: UserCreate
         },
         {
             path: '/users/:userId',
