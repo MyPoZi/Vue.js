856d6fd083c24b3bf52b33ec91cb6e9347b1b845
MyPoZi <kxx555u.u@gmail.com>
2018-10-29 15:41:56 +0900

    add userdetail

--- Vue_introduction/section4/main.js
+++ Vue_introduction/section4/main.js
@@ -15,12 +15,82 @@ var getUsers = function (callback) {
     }, 1000)
 }
 
+var getUser = function (userId, callback) {
+    setTimeout(function () {
+        var filteredUsers = userData.filter(function (user) {
+            return user.id === parseInt(userId, 10)
+        })
+        callback(null, filteredUsers && filteredUsers[0])
+    }, 1000)
+}
+
+var userData = [
+    {
+        id: 1,
+        name: 'MyPoZi',
+        description: 'エンジニアです'
+    },
+    {
+        id: 2,
+        name: 'MyZiPo',
+        description: 'プログラマです'
+    }
+]
+// 詳細ページのコンポーネント
+var UserDetail = {
+    template: `
+    <div>
+        <div class="loading" v-if="loading">読み込み中...</div>
+        <div v-if="error" class="error">
+            {{ error }}
+        </div>
+        <div v-if="user">
+            <h2>{{ user.name }}</h2>
+            <p>{{ user.description }}</p>
+        </div>
+    </div>
+    `,
+    // 初期値のセット
+    data: function () {
+        return {
+            loading: false,
+            user: null,
+            error: null
+        }
+    },
+
+    created: function () {
+        this.fetchData()
+    },
+
+    watch: {
+        '$route': 'fetchData'
+    },
+
+    methods: {
+        fetchData: function () {
+            this.loading = true
+            // this.$route.params.userIdに現在のURL上のパラメータに対応したuserIdが格納される
+            getUser(this.$route.params.userId, (function (err, user) {
+                this.loading = false
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.user = user
+                }
+            }).bind(this))
+        }
+    }
+
+
+}
+
 var UserList = {
     template: `
     <div>
         <div class="loading" v-if="loading">読み込み中</div>
         <div v-if="error" class="error">
-            {{ error}}
+            {{ error }}
         </div>
         <!--usersがロードされたら各ユーザーの名前を表示する-->
         <div v-for="user in users" :key="user.id">
@@ -79,6 +149,10 @@ var router = new VueRouter({
         {
             path: '/users',
             component: UserList
+        },
+        {
+            path: '/users/:userId',
+            component: UserDetail
         }
     ]
 })
