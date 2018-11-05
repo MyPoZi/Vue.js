662b9e44d0297f774fdbf51bc0246b4cc45c0ffe
MyPoZi <kxx555u.u@gmail.com>
2018-11-05 20:12:50 +0900

    add section6

---
+++ Vue_introduction/section6/Root.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="root">
+        <h1 class="header">Rootコンポーネント</h1>
+        <p>これはRootコンポーネント</p>
+        <foo></foo>
+        <bar></bar>
+    </div>
+</template>
+
+<script>
+    import Foo from "./foo";
+    import Bar from "./bar";
+    export default {
+        name: "Root",
+        components: {Bar, Foo}
+    }
+</script>
+
+<style>
+    #root {
+        border: solid 1px blue;
+        margin: 4px;
+        padding: 4px;
+    }
+    .header {
+        font-size: 200%;
+    }
+
+</style>
---
+++ Vue_introduction/section6/foo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="foo">
+        <h1 class="header">Fooコンポーネント</h1>
+        <p>これはFooコンポーネント</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "foo"
+    }
+</script>
+
+<style>
+    p {
+        text-decoration: underline;
+    }
+
+</style>
+
+<style scoped>
+    .foo {
+        border: solid 1px green;
+        margin: 4px;
+        padding: 4px;
+    }
+    .header {
+        font-size: 150%;
+    }
+</style>
